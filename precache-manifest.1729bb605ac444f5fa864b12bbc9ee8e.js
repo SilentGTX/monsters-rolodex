bf398ec8ca0d1909f1c03489fb0b7a87c31813f3
SLNTGTX <slntgtx@gmail.com>
2020-09-29 12:53:08 +0300

    Updates

---
+++ precache-manifest.1729bb605ac444f5fa864b12bbc9ee8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7983e3045b1035c45a70248ffaf042be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "954ad8dc371abc9cdc15",
+    "url": "/monsters-rolodex/static/css/main.084d7c7b.chunk.css"
+  },
+  {
+    "revision": "77263cff91b03fe4e343",
+    "url": "/monsters-rolodex/static/js/2.7b866b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7b866b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954ad8dc371abc9cdc15",
+    "url": "/monsters-rolodex/static/js/main.07040a4d.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
